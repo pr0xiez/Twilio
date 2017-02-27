10a086336bc5adfbbcd4ca4289b41b3877b2b95b
Alex <Alex>
2017-02-27 10:31:40 -0500

    first commit

---
+++ inbound_call.js
@@ -0,0 +1,14 @@
+const http = require('http'); // require web server
+const twilio = require('twilio'); // require Twilio library
+
+http.createServer((req, res) => {
+  const twiml = new twilio.TwimlResponse();
+
+  twiml.say('Welcome to United Installs'); 
+
+  res.writeHead(200, {'Content-Type': 'text/xml'}); // web server details with status code 200
+  res.end(twiml.toString()); // TwiML repsonse from server
+})
+.listen(1337, '127.0.0.1'); // port , IP address of web server
+
+console.log('TwiML server running at http://127.0.0.1:1337/');
---
+++ make_call.js
@@ -0,0 +1,11 @@
+const accountSid = 'ACbc358b5c75b403806147137308afb862';
+const authToken = 'e15d7ff387b8f4bed3cd635df851fdb4';
+const client = require('twilio')(accountSid, authToken);
+
+client.calls
+  .create({
+    url: 'http://demo.twilio.com/docs/voice.xml',
+    to: '+14155551212',
+    from: '+15017250604',
+  })
+  .then((call) => process.stdout.write(call.sid));

a2e86cd274d796ffed28a19c24d0546fcef8b7c4
Alex <Alex>
2017-02-27 15:54:07 -0500

    created different responses based on date and time

--- js/inbound_call.js
+++ js/inbound_call.js
@@ -1,24 +1,56 @@
 "use strict";
 
-const http = require('http'); // require web server
-const twilio = require('twilio'); // require Twilio library
+const http = require('http');
+const twilio = require('twilio');
+
+const holidayResponse = 'THANK YOU FOR CALLING UNITED INSTALLS, YOU HAVE REACHED US DURING A HOLIDAY. THERE IS A POSSIBILITY THAT SOMEONE MAY BE IN THE OFFICE. IF YOU KNOW YOUR PARTIES EXTENTION YOU MAY ENTER IT AT ANY TIME. WE DO ASK THAT YOU CALL BACK AT NORMAL BUSINESS HOURS OF 8 O’CLOCK AM TO 5:00 O’CLOCK PM MONDAY THROUGH FRIDAY';
+const afterHoursResponse = 'THANK YOU FOR CALLING UNITED INSTALLS, YOU HAVE REACHED US EITHER BEFORE OR AFTER NORMAL BUSINESS HOURS. THERE IS A POSSIBILITY THAT SOMEONE MAY STILL BE IN THE OFFICE. IF YOU KNOW YOUR PARTIES EXTENTION YOU MAY ENTER IT AT ANY TIME. WE DO ASK THAT YOU CALL BACK AT NORMAL BUSINESS HOURS OF 8 O’CLOCK AM TO 5 O’CLOCK PM MONDAY THROUGH FRIDAY';
+const mainResponse = 'THANK YOU FOR CALLING UNITED INSTALLS YOUR INDEPENDENT SERVICE PROVIDOR FOR LOWES, TO ENSURE THE HIGHEST LEVEL OF CUSTOMER CARE THIS CALL MAY BE RECORDED. IF YOU KNOW YOUR PARTIES EXTENTION YOU MAY DIAL IT AT ANY TIME';
 
 http.createServer((req, res) => {
   const twiml = new twilio.TwimlResponse();
 
-  twiml.say('Welcome to United Installs'); 
+  twiml.say(getResponse());
 
   res.writeHead(200, {'Content-Type': 'text/xml'}); // web server details with status code 200
 
-  res.end(twiml.toString());
-  /**
-  Outputs the following:
-  <?xml version="1.0" encoding="UTF-8"?>
-  <Response>
-    <Say>Welcome to United Installs</Say>
-  </Response>
-  */
+  res.end(twiml.toString()); // converts string from TwiML response to TwiML
 })
-.listen(1337, '127.0.0.1'); // port , IP address of web server
+.listen(1337, '127.0.0.1'); // port, IP address of web server
 
 console.log('TwiML server running at http://127.0.0.1:1337/');
+
+function getResponse() {
+
+  if (IsHoliday()) {
+    return holidayResponse;
+  }
+  else if (IsWeekend()) {
+    return afterHoursResponse;
+  }
+  else if (IsBusinessHours()) {
+    return mainResponse;
+  }
+  else {
+    return afterHoursResponse;
+  }
+
+}
+
+function IsHoliday() {
+  return false;
+}
+function IsWeekend() {
+  if (Date.prototype.getDay() == 0
+      || Date.prototype.getDay() == 6) 
+      {
+        return true;
+      }
+}
+function IsBusinessHours() {
+  if (Date.prototype.getHours() >= 8
+      && Date.prototype.getHours() < 17) 
+      { 
+        return true;
+      }
+}
--- js/response.js
+++ js/response.js
@@ -1,23 +1,9 @@
 "use strict";
 
-/**
- * @class Response
- * @author Alex Hall <alex.hall@united-installs.com>
- * @description Generates appropriate response to inbound_call for Twilio
- */
-function Response() {
-    const date = Date.now();
+const Response = {
+    function getResponse(){
 
-    /**
-     * @function getResponse
-     * @description returns the correct response to be played back to the caller based on the date and time of day
-     * @returns {String}
-     */
-    function getResponse() {
-        return 'test';
     }
-
-    return { getResponse };
 }
 
 module.exports = Response;

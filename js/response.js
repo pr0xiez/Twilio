d769fc0a451834f1ec93ed61f054dcea54e91ddf
Alex Hall <Alex Hall>
2017-02-28 14:43:43 -0500

    testing node modularization

--- js/response.js
+++ js/response.js
@@ -1,9 +1,23 @@
-"use strict";
+'use strict';
 
-const Response = {
-    function getResponse(){
+/**
+ * @class Response
+ * @author Alex Hall <alex.hall@united-installs.com>
+ * @description Generates appropriate response to inbound_call for Twilio
+ */
+ function Response() {
+    const date = Date.now();
 
-    }
-}
+    /**
+    * @function getResponse
+    * @description returns the correct response to be played back to the caller based on the date and time of day
+    * @returns {String}
+    */
+    function getResponse() {
+        return 'getResponse test';
+     }
+ 
+     return { getResponse };
+ } 
 
 module.exports = Response;
